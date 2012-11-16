4329815f4f73cc451d15d2006e2c7296661cf6d3
Ken Sheedlo <ovrkenthousand@gmail.com>
2012-11-15 19:01:57 -0700

    Include Check framework and update README

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-destructionfx
-=============
+# destructionfx
 
 Destructible 3-D environments - CSCI 4229 Final Project
+Ken Sheedlo
+Fall 2012
+
+destructionfx is a simulated voxel environment designed to facilitate creative
+destruction. It features realistic, efficient collision detection and
+destruction effects.
+
+# Project Setup
+
+The build environment uses the [GDSL](http://home.gna.org/gdsl/) data structures
+library and [Check](http://check.sourceforge.net/), a unit testing framework for
+C. A setup script is included to install dependencies into the local
+environment. To run the initial setup:
+
+    $ ./setup.sh
+
+From there, you should be able to build and run the code.
+
+    $ make
+    $ ./smash
+
+
--- setup.sh
+++ setup.sh
@@ -8,15 +8,15 @@ check_fail () {
     fi
 }
 
-wget http://download.gna.org/gdsl/gdsl-1.6.tar.gz
-tar xzvf gdsl-1.6.tar.gz
-
 WDIR=$(pwd)
 if [[ ! -e env ]]
 then
     mkdir env
 fi
 
+# GDSL
+wget http://download.gna.org/gdsl/gdsl-1.6.tar.gz
+tar xzvf gdsl-1.6.tar.gz
 cd gdsl-1.6
 ./configure --prefix=$WDIR/env
 check_fail "./configure --prefix=$WDIR/env"
@@ -28,3 +28,29 @@ check_fail "make install"
 cd "$WDIR"
 rm gdsl-1.6.tar.gz
 rm -r gdsl-1.6
+
+# Check, a C unit testing framework
+wget http://downloads.sourceforge.net/project/check/check/0.9.8/check-0.9.8.tar.gz
+tar xzvf check-0.9.8.tar.gz
+cd check-0.9.8
+./configure --prefix=$WDIR/env
+check_fail "./configure --prefix=$WDIR/env"
+make 
+check_fail "make"
+make install 
+check_fail "make install"
+
+cd "$WDIR"
+rm check-0.9.8.tar.gz
+rm -r check-0.9.8
+
+echo
+echo "########################################################################"
+echo "#"
+echo "# The project is now ready to build and run. To do so,"
+echo "#"
+echo "#  $ make"
+echo "#  $ ./smash"
+echo "#"
+echo "########################################################################"
+echo
